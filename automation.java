982f1a47c89a22f3f0ae03c128d0c00db020c9c0
divya vandra <divyavandra@gmail.com>
2023-02-25 19:03:03 +0000

    All topics

---
+++ automation.java
@@ -0,0 +1,21 @@
+package Topics;
+
+public class automation {
+
+    static int auto = 123;
+
+    static String automation1 = "examples";
+
+    static final int ALL_MANUAL_TOPICS = 200;
+
+    static final String ALL_STATIC_CONSTANT = "Both are there";
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Both Static Variables and Constant topics are covered here");
+
+
+    }
+}
---
+++ manual.java
@@ -0,0 +1,37 @@
+package Topics;
+
+public class manual {
+
+    int tdd = 40;
+    int bdd = 50;
+
+    int price = 10000;
+
+    // Instance variable practice
+
+    void method1() {
+        int alpha = 10;
+        int beta = 20;
+
+    }
+    void method2() { String smoke = "overall health check up";
+        String sanity = "Specialize health check up";
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("This is Java manual topic");
+        System.out.println(" Covering Local and Instance both variables here");
+
+        int price = 20000;
+
+
+
+
+
+        }
+
+        //This is Local variable practice
+
+}
+
